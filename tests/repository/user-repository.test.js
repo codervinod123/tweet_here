0219b5b902df2dce56eea85fc51a8ad40747d568
vinod kumar <88377243+codervinod123@users.noreply.github.com>
2024-09-30 09:21:24 +0530

    test:users repo tests added

--- tests/repository/user-repository.test.js
+++ tests/repository/user-repository.test.js
@@ -13,11 +13,66 @@ test("Testing that a user is gracefully created or not", async () => {
     updatedAt: "2024-08-06",
   };
 
-  jest.spyOn(User, "create").mockImplementation(() => {
+  const createEntry=jest.spyOn(User, "create").mockImplementation(() => {
     return data;
   });
 
   const userRepository = new UserRepository();
   const user = await userRepository.createEntry(data);
   expect(user.name).toBe(data.name);
+  expect(createEntry).toHaveBeenCalled();
 });
+
+test("Testing that a user can find by email", async()=>{
+    const user={
+      name: "vinod",
+      email: "vinodpr737947@gmail.com",
+      createdAt: "2024-08-06",
+      updatedAt: "2024-08-06",
+    }
+
+    const mockFindOne = jest.spyOn(User,"findOne").mockImplementation(()=>{
+      return user;
+    })
+   
+    // SOME_EMAIL because we are mocking implementation
+    const email="SOME_EMAIL";
+    const userRepository = new UserRepository();
+    const response=await userRepository.getByEmail(email);
+    expect(response.name).toBe(user.name);
+    expect(response.email).toBe(user.email);
+    expect(mockFindOne).toHaveBeenCalled();
+});
+
+test("Testing that user's profile is getting updated or not", async()=>{
+   const profileData={
+        userId:"SOME_ID",
+        profilePic: "SOME_LINK_TO_USER_PROFILE_PIC",
+        name: "Abhishek",
+        bio: "Software Engineer",
+        location: "India",
+   }
+   const mockUpdateProfile=jest.spyOn(User, 'findByIdAndUpdate').mockImplementation(()=>{
+     return true;
+   })
+   const userRepository = new UserRepository();
+   const response=await userRepository.updateProfilePic(profileData.userId, profileData.profilePic, profileData.name, profileData.bio, profileData.location);
+   expect(response).toBeTruthy();
+   expect(mockUpdateProfile).toHaveBeenCalled();
+})
+
+// test('Testing that follower and following is implemented successfully',async()=>{
+//    const usersData={
+//       followerId: "FOLLOWER_ID",
+//       followingId: "FOLLOWING_ID"
+//    }
+
+//    const mockFollowImplementation=jest.spyOn(User, 'updateOne').mockImplementation(()=>{
+//      return true;
+//    })
+//    const userRepository = new UserRepository();
+//    const response=await userRepository.follow(usersData.followerId, usersData.followingId);
+//    console.log(response);
+//    expect(response).toBeTruthy();
+//    expect(mockFollowImplementation).toHaveBeenCalled();
+// })
